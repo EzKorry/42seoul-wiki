a3913081913c9e876880efc0498bb41b67bb810c
ezkorry <eszqsc112@gmail.com>
2020-10-17 14:32:07 +0900

    no auth..

--- server/modules/authentication/42/authentication.js
+++ server/modules/authentication/42/authentication.js
@@ -4,45 +4,85 @@
 // 42 Account
 // ------------------------------------
 
-const FortyTwoStrategy = require('passport-42').Strategy
+const OAuth2Strategy = require('passport-oauth2');
 
 module.exports = {
   init (passport, conf) {
-    passport.use('42',
+    passport.use('42', new OAuth2Strategy({
+      authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
+      tokenURL: 'https://api.intra.42.fr/oauth/token', 
+      clientID: conf.clientId,
+      clientSecret: conf.clientSecret,
+      callbackURL: conf.callbackURL,
+      passReqToCallback: true,
+    },
+      async function(accessToken, refreshToken, profile, cb) {
+        console.log(accessToken);
+        console.log(refreshToken);
+        console.log(profile);
+        conosle.log(cb);
+        try {
+          const user = await WIKI.models.users.processProfile({
+            providerKey: req.params.code,
+            profile
+          });
+          console.log(user);
+          cb(null, user)
+        } catch (err) {
+          console.error(err);
+          cb(err, null)
+        }
+
+        User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+
+  },
+}
+
+
+/*
+const FortyTwoStrategy = require('passport-42').Strategy
+console.log('42 account loading');
+module.exports = {
+  init (passport, conf) {
+    console.dir(conf);
+    //passport.use('42',
+      passport.use(
       new FortyTwoStrategy({
-        clientID: conf.clientId,
-        clientSecret: onf.clientSecret,
-        callbackURL: conf.callbackURL,
+    clientID: conf.clientId,
+    clientSecret: conf.clientSecret,
+    callbackURL: conf.callbackURL,
         // domain: conf.domain,
+
         // clientID: conf.clientId,
         // clientSecret: conf.clientSecret,
         // callbackURL: conf.callbackURL,
-        passReqToCallback: true,
-      }, async (req, accessToken, refreshToken, extraParams, profile, cb) => {
-        try {
-          const user = await WIKI.models.users.processProfile({
-            providerKey: req.params.strategy,
-            profile
-          })
-          cb(null, user)
-        } catch (err) {
-          cb(err, null)
-        }
+        // passReqToCallback: true,
+        // }, (req, accessToken, refreshToken, extraParams, profile, cb) => {
+      }, async (accessToken, refreshToken, profile, cb) => {
+          console.dir(req);
+          console.dir(accessToken);
+          console.dir(refreshToken);
+          console.log(extraParams);
+          console.dir(profile);
       }
       ))
   }
 }
+*/
+        // var FortyTwoStrategy = require('passport-42').Strategy;
 
-// var FortyTwoStrategy = require('passport-42').Strategy;
-
-// passport.use(new FortyTwoStrategy({
-//     clientID: FORTYTWO_APP_ID,
-//     clientSecret: FORTYTWO_APP_SECRET,
-//     callbackURL: "http://127.0.0.1:3000/auth/42/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ fortytwoId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
+        // passport.use(new FortyTwoStrategy({
+        //     clientID: FORTYTWO_APP_ID,
+        //     clientSecret: FORTYTWO_APP_SECRET,
+        //     callbackURL: "http://127.0.0.1:3000/auth/42/callback"
+        //   },
+        //   function(accessToken, refreshToken, profile, cb) {
+        //     User.findOrCreate({ fortytwoId: profile.id }, function (err, user) {
+        //       return cb(err, user);
+        //     });
+        //   }
+        // ));
